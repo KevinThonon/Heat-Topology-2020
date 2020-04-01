c1b183336306952ef9b1dad3e571e953181d08a3
Urban <lemmensurban@gmail.com>
2020-04-02 00:01:49 +0200

    created a matlab file to read the txt files with the solution

--- Topology_test.cpp
+++ Topology_test.cpp
@@ -33,11 +33,11 @@ double myfunc(unsigned n, const double *a, double *grad, void *data){
 	//double cost = top::objective_function1(u, N);
 	//vec lambda = top::lambda1(u, ll, N);
 
-	//double cost = top::objective_function2(u, N);
-	//vec lambda = top::lambda2(ll, N);
+	double cost = top::objective_function2(u, N);
+	vec lambda = top::lambda2(ll, N);
 
-	double cost = top::objective_function3(u, N);
-	vec lambda = top::lambda3(ll, N);
+	//double cost = top::objective_function3(u, N);
+	//vec lambda = top::lambda3(ll, N);
 
 	vec dcda = top::dcda(lambda, u, a, N);
 
--- Visualization.m
+++ Visualization.m
@@ -1,4 +1,39 @@
-f = fopen(metal.txt);
-data = textscan(f,'%s');
-fclose(f);
-variable = str2double(data{1}(2:2:end));
+N = 10;
+
+gradient = 'gradient.txt';
+fileID = fopen(gradient,'r');
+formatSpec = '%f';
+gradient = fscanf(fileID,formatSpec);
+
+metal = 'metal.txt';
+fileID = fopen(metal,'r');
+formatSpec = '%f';
+metal = fscanf(fileID,formatSpec);
+
+temperature = 'temperature.txt';
+fileID = fopen(temperature,'r');
+formatSpec = '%f';
+temperature = fscanf(fileID,formatSpec);
+
+
+gradient_mat = zeros(N,N);
+temperature_mat = zeros(N,N);
+metal_mat = zeros(N,N);
+
+for i = 1:N
+    for j = 1:N
+         gradient_mat(i,j) = gradient((i-1)*N+j);
+         temperature_mat(i,j) = temperature((i-1)*N+j);
+         metal_mat(i,j) = metal((i-1)*N+j);
+    end
+end
+
+
+surf(gradient_mat,'FaceColor','interp')
+
+%surf(temperature_mat,'FaceColor','interp')
+
+%surf(metal_mat,'FaceColor','interp')
+
+
+
