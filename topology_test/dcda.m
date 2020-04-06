9977bb80bf73a18253c0d469acb53072c4884553
philippe111 <philippedewart@gmail.com>
2020-04-06 10:37:57 +0200

    update dcda.m

--- topology_test/dcda.m
+++ topology_test/dcda.m
@@ -8,10 +8,10 @@ penal = 3.0;
 for i = 1:1:N
     for j = 1:1:N
         dKdk_u = zeros((N+1)*(N+1),1);
-        dKdk_u(i + (j-1)*(N+1)) = -1.0*T(i + (j-1)*(N+1)) + 0.5*T(i+1 + (j-1)*(N+1)) + 0.5*T(i + (j)*(N+1));
-        dKdk_u(i+1 + (j-1)*(N+1)) = -1.0*T(i+1 + (j-1)*(N+1)) + 0.5*T(i + (j-1)*(N+1)) + 0.5*T(i+1 + (j)*(N+1));
-        dKdk_u(i + (j)*(N+1)) = -1.0*T(i + (j)*(N+1)) + 0.5*T(i + (j-1)*(N+1)) + 0.5*T(i+1 + (j)*(N+1));
-        dKdk_u(i+1 + (j)*(N+1)) = -1.0*T(i+1 + (j)*(N+1)) + 0.5*T(i+1 + (j-1)*(N+1)) + 0.5*T(i + (j)*(N+1));
+        dKdk_u(i + (j-1)*(N+1)) = -1.0*T(i + (j-1)*(N+1)) + mean(T(i+1 + (j-1)*(N+1)), T(i + (j)*(N+1)));
+        dKdk_u(i+1 + (j-1)*(N+1)) = -1.0*T(i+1 + (j-1)*(N+1)) + mean(T(i + (j-1)*(N+1)), T(i+1 + (j)*(N+1)));
+        dKdk_u(i + (j)*(N+1)) = -1.0*T(i + (j)*(N+1)) + mean(T(i + (j-1)*(N+1)), T(i+1 + (j)*(N+1)));
+        dKdk_u(i+1 + (j)*(N+1)) = -1.0*T(i+1 + (j)*(N+1)) + mean(T(i+1 + (j-1)*(N+1)), T(i + (j)*(N+1)));
         
         dcdk(i+(j-1)*N) = transpose(lambda)*dKdk_u;
         dcdam(i,j) = penal*(65.0-0.2)*pow(pctmetal(i,j),penal-1)*dcdk(i + (j-1)*N);
